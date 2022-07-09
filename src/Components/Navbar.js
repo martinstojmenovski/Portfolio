341c20a9cf8a3c0b882cd8402d95ee4c4dbb86f7
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-07-09 01:35:41 -0600

    Added hamburger function, needs css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,15 +3,21 @@ import Home from './Home'
 import About from './About'
 import Project from './Project'
 import Skills from './Skills'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
 
 function Navbar() {
+
     //useRef declaration for page navigation
     const about = useRef(null)
     const project = useRef(null)
     const skills = useRef(null)
     const home = useRef(null)
+
     //useState to blur navbar
     const [blur, setBlur] = useState(false)
+
+    const [hamburger, setHamburger] = useState(true)
 
 
 
@@ -24,27 +30,37 @@ function Navbar() {
     }
 
     const blurNavbar = () => {
-        if(window.scrollY >= 100){
+        if(window.scrollY >= 80){
             setBlur(true)
         }else {
             setBlur(false)
         }
     }
-
     window.addEventListener('scroll', blurNavbar)
+
+    const showHamburger = () => setHamburger(!hamburger)
+
 
     return (
         <div>
-            <nav className={blur ? 'navbar active' : 'navbar'}>
+            <nav className={blur ? 'navbar active' : 'navbar'} >
                 <div>
                     <h3 className='brand-name' onClick={() => scrollToSection(home)} >Martin Stojmenovski</h3>
                 </div>
                 <div>
+                    {hamburger && <div className='hamburger'>
+                    <FaIcons.FaBars onClick={showHamburger}/>
+                    </div>}
+                    {!hamburger && <div className='sidebar'>
+                    <AiIcons.AiOutlineClose onClick={showHamburger} />
                     <ul className='navigation-list'>
                         <li onClick={() => scrollToSection(about)} >About</li>
                         <li onClick={() => scrollToSection(project)}>Project</li>
                         <li onClick={() => scrollToSection(skills)}>Skills</li>
                     </ul>
+                    
+                    </div> }
+                    
                 </div>
             </nav>
             <Home home={home} />
