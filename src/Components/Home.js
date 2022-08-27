7dea0a7b6b0fbbc8679bb0313c6e6fd3652f1838
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-08-26 23:57:37 -0600

    Homepage organazing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,12 @@ import './Home.css'
 function Home({ home }) {
     return (
       <div ref={home} className="home">
-            <h1 className="brand" ><span>Creative</span><br/>DEVELOPER<span className='and'>&</span><br/>DESIGNER</h1>
+        
+            <p className='creative'>Creative</p>
+            <h1 className="developer" ><br/>DEVELOPER<span className='and'>&</span></h1>
+            <h1 className='designer'>DESIGNER</h1>
             <p className='statement'>I bring my passion for humor and travel into each project, work environment.</p>
+            
             <button className='contact'>Contact me</button>
       </div>
     );
