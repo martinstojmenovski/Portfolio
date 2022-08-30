64eb5489e28ecf71d83a6471c31c3c8cd4ea97a3
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-08-30 16:02:48 -0600

    Some css.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,18 @@
 import './home.css'
 function Home({ home }) {
-    return (
-      <div ref={home} className="home">
-        <div className='logo'>
-            <p className='creative'>&nbsp;creative</p>
-            <h1 className="developer" >DEVELOPER<span className='and'>&</span></h1>
-            <h1 className='designer'>DESIGNER</h1>
-            </div>
-            <p className='statement'>&emsp;Passionate about simple and functional design. I love coffee, architecture and golf.</p>
-            
-            <button className='contact'>Contact me</button>
+  return (
+    <div ref={home} className="home">
+      <div className='logo'>
+        <p className='creative'>&nbsp;creative</p>
+        <h1 className="developer" >DEVELOPER<span className='and'>&</span></h1>
+        <h1 className='designer'>DESIGNER</h1>
       </div>
-    );
-  }
+      <p className='statement'>&emsp;Passionate about simple and functional design. I love coffee, architecture and golf.</p>
 
-    
-  export default Home;
-  
+      <button className='contact'>Contact me</button>
+    </div>
+  );
+}
+
+
+export default Home;
