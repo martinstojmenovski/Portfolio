41038a81bdb43a4980f8e563b9eb7c73249dbdb6
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-08-30 10:51:34 -0600

    Section home css organizing.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,11 @@ import './home.css'
 function Home({ home }) {
     return (
       <div ref={home} className="home">
-        
+        <div className='logo'>
             <p className='creative'>Creative</p>
             <h1 className="developer" >DEVELOPER<span className='and'>&</span></h1>
             <h1 className='designer'>DESIGNER</h1>
+            </div>
             <p className='statement'>I bring my passion for humor and travel into each project, work environment.</p>
             
             <button className='contact'>Contact me</button>
