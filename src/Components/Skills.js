7616a4ab90497e89d9a745c27f0c0c501ba5d972
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-10-19 10:31:33 -0600

    Contact information css style.

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,23 +1,22 @@
 import "./skills.css"
 function Skills({ skills }) {
   return (
-    <section ref={skills} className="CONTACT">
+    <section ref={skills} className="CONTACT" id="CONTACT">
       <main>
-        <div>
-          <h1 >Let's connect</h1>
-          <div className="big-dot"></div>
-        </div>
-        <div ><ul>
-          <a href="mailto:martinstojmenovskim@gmail.com"><li>Email</li></a>
-          <a href="https://www.linkedin.com/in/martin-stojmenovski-4780b9221/" target="_blank" ><li>Linkedin</li></a>
-          <a href="https://github.com/martinstojmenovski?tab=repositories" target="_blank" ><li>Github</li></a>
-        </ul></div>
-
-        <div className='last-words'>
+    
+          <h1 >LET'S CONNECT</h1>
           <p>I am always interested about ux/ui design, frontend development,
             webflow development, digital consultant,
             new businesses, startups, coffee.</p>
-          <div className='dot'></div></div>
+        <ul>
+          <a href="mailto:martinstojmenovskim@gmail.com"><li><div className="dot"><div className="pillar"><div className="wire"></div></div></div>Email</li></a>
+          <a href="https://www.linkedin.com/in/martin-stojmenovski-4780b9221/" target="_blank" ><li><div className="dot"><div className="pillar"><div className="wire"></div></div></div>Linkedin</li></a>
+          <a href="https://github.com/martinstojmenovski?tab=repositories" target="_blank" ><li><div className="dot"><div className="pillar"></div></div>Github</li></a>
+        </ul>
+
+     
+        
+         
 
 
       </main>
