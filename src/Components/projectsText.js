4cdeeafc3aa25247d33a8f94a286e30c75ce6a18
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-10-21 00:38:12 -0600

    Animating dropdown menu

--- src/Components/projectsText.js
+++ src/Components/projectsText.js
@@ -1,50 +1,50 @@
 const project1 = (
   <div className="container1">
-    <a href="https://martinstojmenovski.github.io/Friends/" target="_blank">
+    
       <div className="project1">
       <h5>FRIENDS</h5>
       <p>User authentication application built into React and Django
         allows you to create an account and login with user verification.</p>
-      <span>MORE</span>
+        <a href="https://martinstojmenovski.github.io/Friends/" target="_blank"><span>MORE</span></a>
     </div>
-    </a>
+    
   </div>
 )
 const project2 = (
   <div className="container2">
-    <a href="https://the-javawockeez.github.io/Ventilation/" target="_blank">
+    
       <div className="project2">
       <h5>VENTILATION</h5>
       <p>The intent of this website is to be a social
         media page that allows users to post their grievances. It is team project built in React and Express.js.</p>
-      <span>MORE</span>
+        <a href="https://the-javawockeez.github.io/Ventilation/" target="_blank"><span>MORE</span></a>
     </div>
-    </a>
+    
   </div>
 )
 
 const project3 = (
   <div className="container3">
-    <a href="https://martinstojmenovski.github.io/React-Marvel-api/" target="_blank">
+    
       <div className="project3">
       <h5>MARVEL</h5>
       <p>Interactive application allows you to read the description
         of the given character by selecting it. Built into React using third-party APIs.</p>
-      <span>MORE</span>
+        <a href="https://martinstojmenovski.github.io/React-Marvel-api/" target="_blank"><span>MORE</span></a>
     </div>
-    </a>
+    
   </div>
 )
 
 const project4 = (
   <div className="container4">
-    <a href="https://martinstojmenovski.github.io/portfolio/index.html#aboutMe" target="_blank">
+    
       <div className="project4">
       <h5>PORTFOLIO</h5>
       <p>The portfolio website is my first project created
         to represent myself online. Technologies used for this website: HTML, CSS and jQuery.</p>
-      <span>MORE</span>
-    </div></a>
+        <a href="https://martinstojmenovski.github.io/portfolio/index.html#aboutMe" target="_blank"><span>MORE</span></a>
+    </div>
   </div>
 )
 
