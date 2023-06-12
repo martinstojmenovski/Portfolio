ee29e668d2741c9b7782f7434e01c409ff08baed
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-06-12 14:04:58 -0600

    Function to set a border to the selected page added.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -13,8 +13,6 @@ function Navigation() {
         //useState to blur navbar 
         const [blur, setBlur] = useState("transparent")
 
-
-
     //Page navigation function - overflow hidden to stop page from scrolling
     const scrollToSection = (elementRef) => {
         window.scrollTo({
@@ -27,7 +25,6 @@ function Navigation() {
 
         // blur navigation menu when scroll
     const blurNavbar = () => {
-        console.log(window.scrollY)
         if (window.scrollY >= 30) {
             setBlur("#1b1b1b")  
         } else {
@@ -36,64 +33,52 @@ function Navigation() {
     }
     window.addEventListener('scroll', blurNavbar)
 
+
+
         // Function to set a border to the selected page
-        const sections = document.querySelectorAll('section')
-        const navLi = document.querySelectorAll('nav ul li')
+       
         const makeBorderbox = () => {
+            const sections = document.querySelectorAll('section')
+            const navLi = document.querySelectorAll('nav ul li') 
             let current = '';
             sections.forEach(section => {
                 const sectionTop = section.offsetTop
-                // const sectionHeight = section.clientHeight
                 if (window.scrollY + 450 >= sectionTop) {
                     current = section.getAttribute('class')
                 }
             })
             navLi.forEach(li => {
-                li.classList.remove('border')
+                li.style.border = "2px solid transparent"
                 if (li.classList.contains(current)) {
-                    li.classList.add('border')
+                    li.style.border = "2px solid"
                 }
             })
         }
         window.addEventListener('scroll', makeBorderbox)
 
-        
+
     const styleList = {
         listStyle: "none",
         cursor: "pointer",
         color: "#ececec",
         fontWeight: "700",
-        // padding: "7px 10px",
-        // margin: "10px 10px 10px 10px",
-        // textAlign: "center",
-        border: "2px solid transparent",
+        padding: "7px 10px",
         borderRadius: "5px",
-        // transition: "margin 150ms ease-in-out, padding 200ms ease-in-out",
-        // maxWidth: "100%",
-        // zIndex: "4"
     }
 
-
+const border = {}
 
 
     return (
         <div>
             <nav style={{
-                // background: "rgba(0, 0, 0, 0)",
-                // display: "flex",
-                //    justifyContent: "space-evenly",
-                //    alignItems: "center",
                 background: blur,
-                
                 width: "100%",
                 position: "sticky",
-                // overflow: "hidden",
                 top: "0",
-                
                 margin: "auto",
                 zIndex: "1",
                 transition: "1s"
-                
             }}
             >
                 <ul style={{
@@ -105,7 +90,7 @@ function Navigation() {
                    height: "60px",
                    alignItems: "center",
                 }}>
-                    <li style={styleList} className={'HOME border'} onClick={() => scrollToSection(home)} >HOME</li>
+                    <li style={{...styleList, border: "2px solid"}} className={'HOME'} onClick={() => scrollToSection(home)} >HOME</li>
                     <li style={styleList} className={'PROJECT'} onClick={() => scrollToSection(project)} >WORK</li>
                     <li style={styleList} className={'ABOUT'} onClick={() => scrollToSection(about)} >ABOUT</li>
                     <li style={styleList} className={'CONTACT'} onClick={() => scrollToSection(skills)} >CONTACT</li>
