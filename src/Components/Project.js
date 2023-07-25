7b84c1ddace5f82d20bd5c0f55b0dafca0ec475b
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-07-25 16:17:35 -0600

    Working on project slider for responsive design.

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -12,59 +12,17 @@ function Project({ project, projectsText }) {
   const isDesktopOrLaptop = useMediaQuery({ query: '(min-width:  768px)' })
 
 
-  // function to change previous index of the slider.
-  const goToPrevious = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex)
-  }
-
-  // function to change next index of the slider.
-  const goToNext = () => {
-    const isLastSlide = currentIndex === images.length - 1
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
-
-  // map function to display dots for every index of the array.
-  const dots = images.map((image, slideIndex) => {
-    return (
-      <div
-        key={slideIndex}
-        className={currentIndex === slideIndex ? 'dots' : 'dots active'}
-        onClick={() => goToSlide(slideIndex)} >
-      </div>
-    )
-  })
-
-  // functuin to change the index of the array when click on the dot.
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex)
-  }
-
+ 
 
   return (
     <section ref={project} id='PROJECT' className='PROJECT'>
-      <h1 style={{paddingTop:"100px"}}>EXPLORE PREVIOUS WORK</h1>
+      <h1 style={{paddingTop:"80px"}}>EXPLORE PREVIOUS WORK</h1>
 
       {isDesktopOrLaptop ? <div className='projects'>
         {images.map(((image, index) => <div key={index}>{image}</div>))}
       </div> : <SwiperJsWork  /> } 
 
-      {/* <div className="image-slider"> */}
-
-        {/* <div className="left-arrow" onClick={goToPrevious} ><span></span><span></span></div>
-        <div>
-          {images[currentIndex]}
-          <div className="dot-container">
-            {dots}
-          </div>
-        </div>
-        <div className="right-arrow" onClick={goToNext} ><span></span><span></span></div> */}
-
-        
-
-      {/* </div> */}
+     
 
 
 
--- src/Components/SwiperJsWork.js
+++ src/Components/SwiperJsWork.js
@@ -11,48 +11,39 @@ import { EffectCube, Pagination } from 'swiper/modules';
 
 // import "./project.css"
 import slides from "./projectContent"
+
+
+console.log(document.getElementsByClassName("swiper-pagination-bullet-active-main"))
+// .style.backgroundColor = "orange"
 function SwiperJsWork() {
 
     return (
 
-        <div className='project-swiper'>
-            <div className='body'>
+        <div style={{height: "85vh"}} >
+            <div style={{position: "relative", height: "100%"}}>
             <Swiper
-                effect={'cube'}
-                grabCursor={true}
-                cubeEffect={{
-                    shadow: true,
-                    slideShadows: true,
-                    shadowOffset: 20,
-                    shadowScale: 0.94,
-                }}
-                pagination={true}
-                modules={[EffectCube, Pagination]}
-                className="mySwiper"
+                   pagination={{
+                    dynamicBullets: true,
+                  }}
+                  modules={[Pagination]}
+                  className="mySwiper"
             >
                 {slides.map((slide, index) => (
-                    <SwiperSlide style={{border: "2px solid gray" , display:"flex", flexDirection:"column", alignItems:"center", backgroundColor:"white"}} key={index}>
+                    <SwiperSlide style={{
+                        border: "4px solid rgb(234, 234, 234)" ,
+                        color: "#6a6a6a",
+                         display:"flex",
+                          flexDirection:"column",
+                          alignItems:"center",
+                          justifyContent:"space-around"
+                          }} key={index}>
                   
                         <h5>{slide.name}</h5>
-                    <p style={{textAlign:"center", padding:"30px 10px"}} >{slide.content}</p>
-                      <a  href={slide.link} target="_blank"><span>MORE</span></a>
+                    <p style={{textAlign:"center", padding:"0 10px"}} >{slide.content}</p>
+                      <a style={{paddingBottom:"10px"}} href={slide.link} target="_blank"><span>MORE</span></a>
                       
                       </SwiperSlide>
                 ))}
-                {/* <SwiperSlide >
-                    <div className="container1">
-
-                        <div className="project1">
-                        <h5>FRIENDS</h5>
-                        <p>User authentication application built in React and Django
-                            allows you to create an account and login with user verification.</p>
-                        <a href="https://martinstojmenovski.github.io/Friends/" target="_blank"><span>MORE</span></a>
-                    </div>
-
-                    </div>
-                </SwiperSlide> */}
-
-
             </Swiper>
             </div>
         </div>
