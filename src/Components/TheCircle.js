4909f13021f444c651b5d5cc93c7d6ca85cf1e22
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-08-15 09:06:55 -0600

    Working on rotating curved text.

--- src/Components/TheCircle.js
+++ src/Components/TheCircle.js
@@ -2,28 +2,27 @@ import jsxToString from "jsx-to-string";
 import "./theCircle.css"
 import ReactCurvedText from "react-curved-text";
 
+
 function TheCircle() {
- 
-
-
 
 
   const currentJsx = (
     <ReactCurvedText
-    width={300}
-    height={300}
-    cx={150}
-    cy={150}
-    rx={100}
-    ry={100}
-    startOffset={0}
-    reversed={true}
-    text='react-curved-text'
-    textProps={{"style": {"fontSize": 24}}}
-    textPathProps={null}
-    tspanProps={null}
-    ellipseProps={null}
-    svgProps={{"className": "rotating-curved-text"}}
+      
+      width={600}
+      height={600}
+      cx={300}
+      cy={300}
+      rx={173}
+      ry={173}
+      startOffset={0}
+      reversed={false}
+      text='Development  -  Research  -  UI Design  -  UX Design  -  Creative Ideas  -  Amazing Photo  -  '
+      textProps={{ "style": { "fontSize": 26 } }}
+      textPathProps={{"fill": "#e4e4e45d"}}
+      tspanProps={null}
+      ellipseProps={null}
+      svgProps={{ "className": "rotating-curved-text" }}
     />
   );
 
@@ -34,14 +33,14 @@ function TheCircle() {
   currentJsxString =
     "import ReactCurvedText from 'react-curved-text';\n\n" + currentJsxString;
   return (
-    
- <div className="exampleWrapperDiv">{currentJsx}</div>
-   
-     
 
-   
-   
-    
+    <div className="exampleWrapperDiv">{currentJsx}</div>
+
+
+
+
+
+
   );
 }
 
