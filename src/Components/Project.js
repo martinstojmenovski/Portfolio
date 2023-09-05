4104d4c30a626d7687349d4156fe425dd4ffc2cc
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-09-05 15:20:06 -0600

    Style on big screen in project section completed!!.

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -5,7 +5,7 @@ import { useMediaQuery } from 'react-responsive';
 
 
 function Project({ project }) {
-  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width:  738px)' });
+  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width:  768px)' });
   const isSmallerThan480 = useMediaQuery({ query: '(min-width:  480px)' });
   const isBigScreen = useMediaQuery({ query: '(min-width:  1024px)' });
 
@@ -34,7 +34,7 @@ function Project({ project }) {
     for (let i = 0; i < descriptions.length; i++) {
       let windowheight = window.innerHeight;
       let descriptionstop = descriptions[i].getBoundingClientRect().top;
-      let revealpoint = 100;
+      let revealpoint = 0;
       if (descriptionstop < windowheight - revealpoint) {
     
         descriptions[i].classList.add('active')
@@ -59,35 +59,46 @@ function Project({ project }) {
       {slides.map((slide, index) =>
 
       (
-        <div key={index} style={{
+        <a href={slide.link} key={index} style={{
+  
            display: "flex",
             flexDirection: index % 2 === 0 ? "row" : "row-reverse", 
             justifyContent: "center",
              overflow: "visible", 
              margin: isBigScreen ? "80px auto" : "50px auto" ,
-            position:"relative",
-             marginRight: isBigScreen ? index % 2 === 0 ? "0" : "100px" : null, // Add margin-left for the second element
-             marginLeft: isBigScreen ? index % 2 === 0 ? "100px" : "0" : null, // Add margin-right for the first element
+           
+             marginRight: isDesktopOrLaptop ? index % 2 === 0 ? "0" : "40%" : null, // Add margin-left for the second element
+             marginLeft: isDesktopOrLaptop ? index % 2 === 0 ? "40%" : "0" : null, // Add margin-right for the first element
              
              
           
              }}>
           <div className="descriptions"
             style={{
+              zIndex:"22",
               padding: "8px",
               textAlign: index % 2 === 0 ? "right" : "left",
-              display:isDesktopOrLaptop? "block" : "none",
+              // display:isDesktopOrLaptop? "block" : "none",
+              overflow:"visible",
+              width:"80%",
+              // marginLeft: isDesktopOrLaptop ? index % 2 === 0 ? "0" : "730px" : null, // Add margin-left for the second element
+              // marginRight: isDesktopOrLaptop ? index % 2 === 0 ? "700px" : "0" : null, // Add margin-right for the first element
+              
+            marginLeft: isDesktopOrLaptop ? index % 2 === 0 ? "0" : "80%" : null , // Add margin-left for the second element
+             marginRight: isDesktopOrLaptop ? index % 2 === 0 ? "75%" : "0" : null, // Add margin-right for the first element
+              
             }}>
             <h3 style={{
               transform: "scale(1, 3)", fontSize: "2.3rem", fontWeight: "bold", position: "relative",
-              top: "30%"
+              top: "30%",
+               
             }} >{slide.name} </h3>
             <p style={{
               position: "relative",
               top: "35%"
             }}>{slide.content}</p>
           </div>
-          <div
+          <div 
             className="image-style"
             style={{backgroundImage: `linear-gradient(rgb(20,30,48, 0.7), rgb(36,59,85, 0.7)), url(${slide.picture})`}}
           >
@@ -96,7 +107,7 @@ function Project({ project }) {
 
 
           </div>
-        </div>
+        </a>
 
       ))}
     </section>
